9b0556157a68bd0437a4da36c3cc4e7b29cbe6be
ThiagoHFMC <thiago.frois.cunha@gmail.com>
2023-12-01 21:08:48 -0300

    feat: Solution for the first challenge
    This commit contains the two codes for the first challenge.
    Find the sum of all numbers present in a text.

---
+++ day_01/trebuchet.py
@@ -0,0 +1,21 @@
+with open('input.txt', 'r') as file:
+    lines = file.readlines()
+
+calibration_sum = 0
+
+list_one = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+list_two = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+enum_numbers = dict(zip(list_one, list_two))
+
+def map_string_to_number(substring):
+    if substring in list_one:
+        return int(enum_numbers['one'])
+
+for line in lines:
+    for number in enum_numbers.keys():
+        line = line.replace(number, number[0] + str(enum_numbers[number]) + number[-1])
+    lineDigit = list(filter(lambda caractere: caractere.isdigit(), line))
+    calibration_sum += int(lineDigit[0])*10 + int(lineDigit[-1])
+
+print(calibration_sum)
